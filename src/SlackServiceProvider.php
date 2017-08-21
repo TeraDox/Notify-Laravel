aca3be91535a9d8a6f75a64f08b68174e23c44b1
r.ikeda <r.ikeda@teradox.net>
2017-08-21 19:11:52 +0900

    Fixed SlackServiceProvider to publish slack.php when it doesn't exist.

--- src/SlackServiceProvider.php
+++ src/SlackServiceProvider.php
@@ -24,25 +24,28 @@ class SlackServiceProvider extends ServiceProvider
     {
     	if ( file_exists(config_path('slack.php'))) {
 		    $this->mergeConfigFrom( config_path( 'slack.php' ), 'slack' );
-	    }
 
-        $this->app->singleton('maknz.slack', function($app)
-        {
-            return new Client(
-                $app['config']->get('slack.endpoint'),
-                [
-                    'channel' => $app['config']->get('slack.channel'),
-                    'username' => $app['config']->get('slack.username'),
-                    'icon' => $app['config']->get('slack.icon'),
-                    'link_names' => $app['config']->get('slack.link_names'),
-                    'unfurl_links' => $app['config']->get('slack.unfurl_links'),
-                    'unfurl_media' => $app['config']->get('slack.unfurl_media'),
-                    'allow_markdown' => $app['config']->get('slack.allow_markdown'),
-                    'markdown_in_attachments' => $app['config']->get('slack.markdown_in_attachments')
-                ],
-                new Guzzle
-            );
-        });
+            $this->app->singleton('maknz.slack', function ($app) {
+                return new Client(
+                    $app['config']->get('slack.endpoint'),
+                    [
+                        'channel' => $app['config']->get('slack.channel'),
+                        'username' => $app['config']->get('slack.username'),
+                        'icon' => $app['config']->get('slack.icon'),
+                        'link_names' => $app['config']->get('slack.link_names'),
+                        'unfurl_links' => $app['config']->get('slack.unfurl_links'),
+                        'unfurl_media' => $app['config']->get('slack.unfurl_media'),
+                        'allow_markdown' => $app['config']->get('slack.allow_markdown'),
+                        'markdown_in_attachments' => $app['config']->get('slack.markdown_in_attachments')
+                    ],
+                    new Guzzle
+                );
+            });
+	    } else {
+            $provider = new \Maknz\Slack\SlackServiceProvider($this->app);
+            $provider->boot();
+        }
+
 
         $this->app->bind(\Maknz\Slack\Client::class, 'maknz.slack');
     }
